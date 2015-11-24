34ee102c426a1c6d7049509dede68cb3c38b65e2
Sam Lin <maxisam@gmail.com>
2015-11-24 12:00:51 -0600

    initial commit

---
+++ FactoryHelper.ts
@@ -0,0 +1,27 @@
+/* tslint:disable:typedef comment-format */
+
+module UI {
+    "use strict";
+
+    export class DirectiveFactory {
+        static GetFactoryFor<T extends ng.IDirective>(classType: Function): ng.IDirectiveFactory {
+            var factory = (...args): T => {
+                var directive = <any>classType;
+                return new directive(...args); //Typescript 1.6 (return new (directive.bind(directive, ...args));)
+            };
+            factory.$inject = classType.$inject;
+            return factory;
+        }
+    }
+
+    export class GeneralFactory {
+        static GetFactoryFor<T>(ClassType: Function): any {
+            var factory = (...args): T => {
+                var cType = <any>ClassType;
+                return new cType(...args);  //Typescript 1.6 (return new (cType.bind(cType, ...args));)
+            };
+            factory.$inject = ClassType.$inject;
+            return factory;
+        }
+    }
+}
